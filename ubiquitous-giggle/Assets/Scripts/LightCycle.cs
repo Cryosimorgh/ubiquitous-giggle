080ff39fa7c709537be464beadacdb9ada3936e5
sgt-buggerov <amir.h.manavi.r@gmail.com>
2021-06-12 17:04:54 +0430

    GeneratorMatBloom&Lighting

--- ubiquitous-giggle/Assets/Scripts/LightCycle.cs
+++ ubiquitous-giggle/Assets/Scripts/LightCycle.cs
@@ -4,11 +4,11 @@ public class LightCycle : MonoBehaviour
 {
     void Start()
     {
-        InvokeRepeating(nameof(DayTimer), 0, 0.1f);
+        InvokeRepeating(nameof(DayTimer), 0, 0.05f);
     }
 
     private void DayTimer()
     {
-        transform.Rotate(0.15f, 0, 0);
+        transform.Rotate(0.075f, 0, 0);
     }
 }
